71f20477ba8e9d6a48bc5bf793d984d2edf2fc3c
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-06-04 16:26:40 +0900

    .

--- Direct2D/Direct2D/Main.cpp
+++ Direct2D/Direct2D/Main.cpp
@@ -3,11 +3,11 @@
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-    GameApp gameApp(1920, 1080);
+    DemoApp demoApp(1920, 1080);
 
-    gameApp.Init();
-    gameApp.Loop();
-    gameApp.UnInit();
+    demoApp.Init();
+    demoApp.Loop();
+    demoApp.UnInit();
 
     return (int)1;
 }
