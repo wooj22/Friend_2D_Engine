72007a5f7c15317a946e04d1cb2feabebc09369c
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-06-12 21:03:46 +0900

    Transform 컴포넌트

--- Direct2D/Direct2D_EngineLib/Transform.cpp
+++ Direct2D/Direct2D_EngineLib/Transform.cpp
@@ -1,1 +1,26 @@
 #include "Transform.h"
+
+// SetParent
+void Transform::SetParent(Transform* newParent)
+{
+    parent = newParent;
+    if (parent)
+        parent->children.push_back(this);
+}
+
+// Local Tansform get
+D2D1_MATRIX_3X2_F Transform::GetLocalMatrix() const
+{
+    return D2D1::Matrix3x2F::Scale(scale.x, scale.y) *
+        D2D1::Matrix3x2F::Rotation(rotation) *
+        D2D1::Matrix3x2F::Translation(position.x, position.y);
+}
+
+// World Tansform get
+D2D1_MATRIX_3X2_F Transform::GetWorldMatrix() const
+{
+    if (parent)
+        return parent->GetWorldMatrix() * GetLocalMatrix();
+    else
+        return GetLocalMatrix();
+}
--- Direct2D/Direct2D_EngineLib/Transform.h
+++ Direct2D/Direct2D_EngineLib/Transform.h
@@ -1,5 +1,28 @@
-#pragma once
+#include <d2d1.h>
+#include <vector>
+
 class Transform
 {
-};
+private:
+    D2D1_POINT_2F position;
+    float rotation;             // degrees
+    D2D1_POINT_2F scale;
 
+    Transform* parent = nullptr;
+    std::vector<Transform*> children;
+
+public:
+    Transform() : position{ 0.0f, 0.0f }, rotation(0.0f), 
+        scale{ 1.0f, 1.0f }, parent(nullptr) {}
+
+    void SetPosition(float x, float y) { position = { x, y }; }
+    void SetRotation(float angle) { rotation = angle; }
+    void SetScale(float scaleX, float scaleY) { scale = { scaleX, scaleY }; }
+    D2D1_POINT_2F GetPosition() const { return position; }
+    float GetRotation() const { return rotation; }
+    D2D1_POINT_2F GetScale() const { return scale; }
+
+    void SetParent(Transform* newParent);
+    D2D1_MATRIX_3X2_F GetLocalMatrix() const;
+    D2D1_MATRIX_3X2_F GetWorldMatrix() const;
+};
