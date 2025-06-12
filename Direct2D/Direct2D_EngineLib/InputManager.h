52fbc639b015a5a9e92bb19a76eee48b326cc717
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-06-12 20:40:39 +0900

    TimeManager, InputManager 엔진 라이브러리 추가

---
+++ Direct2D/Direct2D_EngineLib/InputManager.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <windows.h>
+#include "Singleton.h"
+
+class InputManager : public Singleton<InputManager>
+{
+private:
+	HWND hWnd;
+	POINT mouseClient;
+	SHORT prevState[256] = { 0 };
+	SHORT currState[256] = { 0 };
+
+public:
+	InputManager() = default;
+	~InputManager() override = default;
+
+	void Init(HWND hWnd);
+	void Update();
+	void Release() {};
+
+	bool GetKey(int vKey);
+	bool GetKeyDown(int vKey);
+	bool GetKeyUP(int vKey);
+
+	friend class Singleton<InputManager>;
+};
