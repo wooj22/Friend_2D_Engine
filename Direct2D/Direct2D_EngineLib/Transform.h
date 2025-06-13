cfed92cb29c95a52a46d04aa6dd2b0f5b32288e2
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-06-13 12:49:22 +0900

    TODO :: rotation center 지정 (transform에 멤버 추가), Camera

--- Direct2D/Direct2D_EngineLib/Transform.h
+++ Direct2D/Direct2D_EngineLib/Transform.h
@@ -43,21 +43,25 @@ public:
     Transform() : position{ 0.0f, 0.0f }, rotation(0.0f), 
         scale{ 1.0f, 1.0f }, parent(nullptr) { localMatrix = D2D1::Matrix3x2F::Identity(); }
 
+    void SetParent(Transform* newParent);
+    void ResetTransform();
+
+    // set
     void SetPosition(float x, float y) { isDirty = true; position = { x, y }; }
     void SetRotation(float angle) { isDirty = true; rotation = angle; }
     void SetScale(float scaleX, float scaleY) { isDirty = true; scale = { scaleX, scaleY }; }
     
+    // add
     void AddPosition(float x, float y) { isDirty = true; position.x += x; position.y += y; }
     void AddRotation(float angle) { isDirty = true; rotation += angle; }
     void AddScale(float scaleX, float scaleY) { isDirty = true; scale.x += scaleX; scale.y += scaleY; }
     
+    // get
     D2D1_POINT_2F GetPosition() const { return position; }
     float GetRotation() const { return rotation; }
     D2D1_POINT_2F GetScale() const { return scale; }
 
-    void SetParent(Transform* newParent);
-    void ResetTransform();
-
+    // get matrix
     D2D1::Matrix3x2F GetLocalMatrix();
     D2D1::Matrix3x2F GetWorldMatrix();
     D2D1::Matrix3x2F GetScreenMatrix();
