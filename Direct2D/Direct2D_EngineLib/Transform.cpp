c454d3be87625d6293f42e60359eb14a25018f28
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-06-13 11:58:25 +0900

    Transform 수정

--- Direct2D/Direct2D_EngineLib/Transform.cpp
+++ Direct2D/Direct2D_EngineLib/Transform.cpp
@@ -1,8 +1,13 @@
 #include "Transform.h"
 
-D2D1_MATRIX_3X2_F Transform::cameraInversMatrix = D2D1::Matrix3x2F::Identity();
-D2D1_MATRIX_3X2_F Transform::unityMatrix = D2D1::Matrix3x2F::Identity();
-D2D1_MATRIX_3X2_F Transform::renderMatrix = D2D1::Matrix3x2F::Scale(0, -1.0f);
+// Game App에서 초기화 필요
+// 일단 카메라 움직임 없고, (1024, 768) 기준으로 수동 초기화
+D2D1::Matrix3x2F Transform::cameraInversMatrix = 
+    D2D1::Matrix3x2F::Identity();
+D2D1::Matrix3x2F Transform::unityMatrix = 
+    D2D1::Matrix3x2F::Translation(1024.0f / 2.0f, 768.0f / 2.0f);
+D2D1::Matrix3x2F Transform::renderMatrix =
+D2D1::Matrix3x2F::Scale(1.0f, -1.0f);
 
 // SetParent
 void Transform::SetParent(Transform* newParent)
@@ -12,26 +17,40 @@ void Transform::SetParent(Transform* newParent)
         parent->children.push_back(this);
 }
 
-// Local Tansform get
-D2D1_MATRIX_3X2_F Transform::GetLocalMatrix() const
+// Reset
+void Transform::ResetTransform()
 {
-    return D2D1::Matrix3x2F::Scale(scale.x, scale.y) *
-        D2D1::Matrix3x2F::Rotation(rotation) *
-        D2D1::Matrix3x2F::Translation(position.x, position.y);
+    position = { 0.0f, 0.0f };
+    rotation = { 0.0f };
+    scale = { 1.0f, 1.0f };
+    isDirty = true;
+}
+
+// Local Tansform get
+D2D1::Matrix3x2F Transform::GetLocalMatrix()
+{
+    if (isDirty) {
+        localMatrix = D2D1::Matrix3x2F::Scale(scale.x, scale.y) *
+            D2D1::Matrix3x2F::Rotation(rotation) *
+            D2D1::Matrix3x2F::Translation(position.x, position.y);
+
+        isDirty = false;
+    }
+    
+    return localMatrix;
 }
 
 // World Tansform get
 // World = Local * ParentWorld;
-D2D1_MATRIX_3X2_F Transform::GetWorldMatrix() const
+D2D1::Matrix3x2F Transform::GetWorldMatrix()
 {
-    if (parent)
-        return parent->GetWorldMatrix() * GetLocalMatrix();
-    else
-        return GetLocalMatrix();
+    auto worldMatrix = GetLocalMatrix();
+    worldMatrix = parent ? worldMatrix * parent->GetWorldMatrix() : worldMatrix;
+    return worldMatrix;
 }
 
 // Screen Tansform get
-D2D1_MATRIX_3X2_F Transform::GetScreenMatrix() const
+D2D1::Matrix3x2F Transform::GetScreenMatrix()
 {
-    
+    return renderMatrix * GetWorldMatrix() * cameraInversMatrix * unityMatrix;
 }
--- Direct2D/Direct2D_EngineLib/Transform.h
+++ Direct2D/Direct2D_EngineLib/Transform.h
@@ -1,17 +1,22 @@
-#include <d2d1.h>
-#include <vector>
-
-/* Transform Component*/
-// World = this->local * parent->world
-// Screen = this->world * camera 역행렬
+// World = local * parent->world
+// Screen = world * camera 역행렬
 
 // Uniny Style Screen position
 // 좌표계 - 중앙 (0,0), 왼쪽위(-, +), 오른쪽 아래(+, -)
 // D2D에서 유니티 좌표계처럼 사용하려면 꼼수가 필요함
-// Screen position = renderMatrix * this->world * camera 역행렬 * unity matrix
+// Screen = renderMatrix * world * camera 역행렬 * unity matrix
+//  unity matrix는 중심 이동과 y 반전에 대항 행렬값을 가지고 있다.
 //  renderMatrix는 unity matrix에 대응하기 위해 y scale에 -1을 준 행렬
-//  transform멤버에 저장하면 자식까지 누적 영향을 미치기 때문에 World 계산 직전에 연산만 해야한다.
+//  transform 멤버에 저장하면 자식까지 누적 영향을 미치기 때문에 연산에만 사용해야한다.
 
+// Dirty 패턴
+// transform이 변경에 대한 flag를 활용하여 
+// 매 프레임마다 불필요하게 행렬을 다시 계산하지 않도록 하는 방식
+
+#include <d2d1_1.h>
+#include <vector>
+
+/* Transform Component*/
 class Transform
 {
 private:
@@ -24,25 +29,31 @@ private:
     Transform* parent = nullptr;
     std::vector<Transform*> children;
 
+    // dirty patten
+    bool isDirty = true;
+    D2D1::Matrix3x2F localMatrix;
+
 public:
     // matrix
-    static D2D1_MATRIX_3X2_F cameraInversMatrix;
-    static D2D1_MATRIX_3X2_F unityMatrix;
-    static D2D1_MATRIX_3X2_F renderMatrix;
+    static D2D1::Matrix3x2F cameraInversMatrix;   // screen size 필요
+    static D2D1::Matrix3x2F unityMatrix;          // screen size 필요
+    static D2D1::Matrix3x2F renderMatrix;
 
 public:
     Transform() : position{ 0.0f, 0.0f }, rotation(0.0f), 
-        scale{ 1.0f, 1.0f }, parent(nullptr) {}
+        scale{ 1.0f, 1.0f }, parent(nullptr) { localMatrix = D2D1::Matrix3x2F::Identity(); }
 
-    void SetPosition(float x, float y) { position = { x, y }; }
-    void SetRotation(float angle) { rotation = angle; }
-    void SetScale(float scaleX, float scaleY) { scale = { scaleX, scaleY }; }
+    void SetPosition(float x, float y) { isDirty = true; position = { x, y }; }
+    void SetRotation(float angle) { isDirty = true; rotation = angle; }
+    void SetScale(float scaleX, float scaleY) { isDirty = true; scale = { scaleX, scaleY }; }
     D2D1_POINT_2F GetPosition() const { return position; }
     float GetRotation() const { return rotation; }
     D2D1_POINT_2F GetScale() const { return scale; }
 
     void SetParent(Transform* newParent);
-    D2D1_MATRIX_3X2_F GetLocalMatrix() const;
-    D2D1_MATRIX_3X2_F GetWorldMatrix() const;
-    D2D1_MATRIX_3X2_F GetScreenMatrix() const;
+    void ResetTransform();
+
+    D2D1::Matrix3x2F GetLocalMatrix();
+    D2D1::Matrix3x2F GetWorldMatrix();
+    D2D1::Matrix3x2F GetScreenMatrix();
 };
