d5f9b6fe37192460b1ba0afa7cff458f3bd20047
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-06-13 11:05:24 +0900

    IRenderer 인터페이스를 만드는 이유 주석

--- Direct2D/Direct2D_EngineLib/IRenderer.h
+++ Direct2D/Direct2D_EngineLib/IRenderer.h
@@ -1,4 +1,12 @@
 #pragma once
+
+// Q. 굳이 IRenderer를 나누는 이유
+// A. 확장성 - 현재는 상속관계가 BitmapRenderer밖에 없지만 향후 추가될 가능성이 높음
+//          - ShapeRenderer, TextRenderer, SpriteRenderer, EffectRenderer, ParticleRenderer 등
+//    의존성 - Render(), UnInit()을 호출하는 RenderManager에서 
+//            이 함수 외의 BitmapRenderer멤버에 접근할 일이 없기 때문에 추상 인터페이스를 활용하여 결합도를 낮춤
+
+/* 인터페이스 */
 class IRenderer
 {
 public : 
