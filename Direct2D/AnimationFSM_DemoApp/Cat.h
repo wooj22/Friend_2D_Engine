0764ea258912fd1d732232ad8782935d63317196
woojeong <77192050+wooj22@users.noreply.github.com>
2025-07-05 22:07:59 +0900

    게임콘텐츠 AnimationClip, State, AnimatorController Asset 정의

--- Direct2D/AnimationFSM_DemoApp/Cat.h
+++ Direct2D/AnimationFSM_DemoApp/Cat.h
@@ -2,11 +2,11 @@
 #include "../Direct2D_EngineLib/GameObject.h"
 #include "../Direct2D_EngineLib/Transform.h"
 #include "../Direct2D_EngineLib/SpriteRenderer.h"
-#include "CatController.h"
 #include "../Direct2D_EngineLib/Time.h"
+#include "../Direct2D_EngineLib/Animator.h"
 
-// test
-#include "CatIdleClip.h"
+#include "CatController.h"
+#include "CatAnimatorController.h"
 
 class Cat : public GameObject
 {
@@ -15,59 +15,26 @@ public:
 	SpriteRenderer* spriteRenderer;
 	CatController* controller;
 
-	// animation test
-	CatIdleClip* idleClip = nullptr;
-	float animationTime = 0.0f;
-	int currentFrameIndex = 0;
-
 	Cat() : GameObject("Cat")
 	{
 		transform = AddComponent<Transform>();
 		spriteRenderer = AddComponent<SpriteRenderer>();
 		controller = AddComponent<CatController>();
 	}
-	~Cat() override { delete idleClip; }
+	~Cat() override {  }
 
 	/* GameObject Cycle */
 	void Awake() override				 // 오브젝트가 생성되고 모든 컴포넌트의 OnEnable()이 실행된 이후
 	{
 		// resource create
-		/*auto catTexture = ResourceManager::Get().CreateTexture2D("../Resource/Texture/Cat_Idle.png");
+		auto catTexture = ResourceManager::Get().CreateTexture2D("../Resource/Texture/Cat_Idle.png");
 		auto new_sprite = ResourceManager::Get().CreateSprite(catTexture, "Cat_Idle");
-		spriteRenderer->sprite = new_sprite;*/
-
-		// animation test
-		idleClip = new CatIdleClip();
-		spriteRenderer->sprite = idleClip->frames[0].sprite;
-		transform->SetScale(5, 5);
+		spriteRenderer->sprite = new_sprite;
 	}
 	void SceneStartInit() override {}	 // Scene의 Start
 	void Update() override				 // Scene의 Update
 	{
-		animationTime += Time::GetDeltaTime();
-
-		if (animationTime >= idleClip->duration)
-		{
-			if (idleClip->loop)
-				animationTime = 0.0f;
-			else
-				animationTime = idleClip->duration;
-		}
-
-		int frameIndex = 0;
-		for (size_t i = 0; i < idleClip->frames.size(); ++i)
-		{
-			if (animationTime >= idleClip->frames[i].time)
-				frameIndex = (int)i;
-			else
-				break;
-		}
-
-		if (frameIndex != currentFrameIndex)
-		{
-			currentFrameIndex = frameIndex;
-			spriteRenderer->sprite = idleClip->frames[currentFrameIndex].sprite;
-		}
+		
 	}
 	void Destroyed() override {}		 // Scene의 Exit, GameObject Delete
 };
---
+++ Direct2D/AnimationFSM_DemoApp/CatAnimatorController.h
@@ -0,0 +1,120 @@
+#pragma once
+#include "../Direct2D_EngineLib/AnimationClip.h"
+#include "../Direct2D_EngineLib/AnimationBaseState.h"
+#include "../Direct2D_EngineLib/AnimatorController.h"
+#include "../Direct2D_EngineLib/ResourceManager.h"
+
+/*------------ Animation Clip ------------*/
+class CatIdleClip : public AnimationClip
+{
+public:
+    CatIdleClip()
+    {
+        auto texture = ResourceManager::Get().CreateTexture2D("../Resource/Texture/Cat_Idle.png");
+        LoadSpritesFromJson(texture, "../Resource/Data/SpriteSheet/Cat_Idle_Sprites.json");
+        LoadAnimationClipFromJson(texture, "../Resource/Data/AnimationClip/Cat_Idle_AniClip.json");
+    }
+    ~CatIdleClip() override {}
+};
+
+class CatWalkClip : public AnimationClip
+{
+public:
+    CatWalkClip()
+    {
+        auto texture = ResourceManager::Get().CreateTexture2D("../Resource/Texture/Cat_Walk.png");
+        LoadSpritesFromJson(texture, "../Resource/Data/SpriteSheet/Cat_Walk_Sprites.json");
+        LoadAnimationClipFromJson(texture, "../Resource/Data/AnimationClip/Cat_Walk_AniClip.json");
+    }
+    ~CatWalkClip() override {}
+};
+
+class CatRunClip : public AnimationClip
+{
+public:
+    CatRunClip()
+    {
+        auto texture = ResourceManager::Get().CreateTexture2D("../Resource/Texture/Cat_Run.png");
+        LoadSpritesFromJson(texture, "../Resource/Data/SpriteSheet/Cat_Run_Sprites.json");
+        LoadAnimationClipFromJson(texture, "../Resource/Data/AnimationClip/Cat_Run_AniClip.json");
+    }
+    ~CatRunClip() override {}
+};
+
+/*------------ Animation State ------------*/
+class CatIdleState : public AnimationBaseState
+{
+public:
+    CatIdleState(AnimationClip* clip) : AnimationBaseState(clip) {}
+
+    void Enter() override {}
+    void Update(float dt) override {}
+    void Exit() override {}
+};
+
+class CatWalkState : public AnimationBaseState
+{
+public:
+    CatWalkState(AnimationClip* clip) : AnimationBaseState(clip) {}
+
+    void Enter() override {}
+    void Update(float dt) override {}
+    void Exit() override {}
+};
+
+class CatRunState : public AnimationBaseState
+{
+public:
+    CatRunState(AnimationClip* clip) : AnimationBaseState(clip) {}
+
+    void Enter() override {}
+    void Update(float dt) override {}
+    void Exit() override {}
+};
+
+/*------------ Animation Controller ------------*/
+class CatAnimatorController : public AnimatorController
+{
+public:
+    // Clips
+    CatIdleClip* idleClip = nullptr;
+    CatWalkClip* walkClip = nullptr;
+    CatRunClip* runClip = nullptr;
+
+    // States
+    CatIdleState* idleState = nullptr;
+    CatWalkState* walkState = nullptr;
+    CatRunState* runState = nullptr;
+
+public:
+    CatAnimatorController()
+    {
+        // Clip 积己
+        idleClip = new CatIdleClip();
+        walkClip = new CatWalkClip();
+        runClip = new CatRunClip();
+
+        // State 积己
+        idleState = new CatIdleState(idleClip);
+        walkState = new CatWalkState(walkClip);
+        runState = new CatRunState(runClip);
+
+        // 檬扁 惑怕
+        ChangeAnimation(idleState);
+    }
+
+    ~CatAnimatorController() override
+    {
+        delete idleClip;
+        delete walkClip;
+        delete runClip;
+
+        delete idleState;
+        delete walkState;
+        delete runState;
+    }
+
+    void ToIdle() { ChangeAnimation(idleState); }
+    void ToWalk() { ChangeAnimation(walkState); }
+    void ToRun() { ChangeAnimation(runState); }
+};
