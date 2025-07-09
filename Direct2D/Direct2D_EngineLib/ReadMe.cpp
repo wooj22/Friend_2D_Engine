433efcd6d0a27252428a5d3f7ff4f0d9f844d0d7
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-07-09 21:39:24 +0900

    --------------------------

--- Direct2D/Direct2D_EngineLib/ReadMe.cpp
+++ Direct2D/Direct2D_EngineLib/ReadMe.cpp
@@ -1,19 +1,24 @@
 
+{
+	/* [GameObject Cycle] */
+	virtual void Awake() {};           // 오브젝트가 생성될 때, 생성자 이후
+	virtual void SceneStart() {};      // Scene의 Start -> Update중 SceneStart() 호출 보장 x
+	virtual void Update() {};          // Scene의 Update
+	virtual void Destroyed() {};       // Scene의 Exit, GameObject Delete
+}
 
-/* GameObject Cycle */
-virtual void Awake() {};           // 오브젝트가 생성될 때, 생성자 이후
-virtual void SceneStart() {};      // Scene의 Start -> Update중 SceneStart() 호출 보장 x
-virtual void Update() {};          // Scene의 Update
-virtual void Destroyed() {};       // Scene의 Exit, GameObject Delete
+{
+	/* [Component Cycle] */
+	virtual void OnEnable() = 0;	   // 컴포넌트 활성화시
+	virtual void OnDestroy() = 0;	   // 컴포넌트 or 게임오브젝트 파괴시
+}
 
-/* Component Cycle */
-virtual void OnEnable() = 0;	   // 컴포넌트 활성화시
-virtual void OnDestroy() = 0;	   // 컴포넌트 or 게임오브젝트 파괴시
-
-/* script component cycle */
-void OnEnable() override {}        // 컴포넌트 활성화시
-virtual void Awake() {}            // 오브젝트의 모든 컴포넌트가 생성된 직후 시점 1회 호출
-virtual void Start() {}            // Awake() 이후 시점 1회 호출
-virtual void Update() {}           // 프레임 단위 반복 호출
-virtual void FixedUpdate() {}      // 물리 업데이트 0.02f 보장 반복 호출
-void OnDestroy() override {}       // 컴포넌트 or 오브젝트 소멸 시점
+{
+	/* [script component cycle] */
+	void OnEnable() override {}        // 컴포넌트 활성화시
+	virtual void Awake() {}            // 오브젝트의 모든 컴포넌트가 생성된 직후 시점 1회 호출
+	virtual void Start() {}            // Awake() 이후 시점 1회 호출
+	virtual void Update() {}           // 프레임 단위 반복 호출
+	virtual void FixedUpdate() {}      // 물리 업데이트 0.02f 보장 반복 호출
+	void OnDestroy() override {}       // 컴포넌트 or 오브젝트 소멸 시점
+}
