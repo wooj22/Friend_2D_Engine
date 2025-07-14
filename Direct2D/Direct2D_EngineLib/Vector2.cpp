7bf48a37e22cff82bd2e97bdfa56ef559add5ad3
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-07-14 15:59:43 +0900

    vector 유틸리티 함수 추가

--- Direct2D/Direct2D_EngineLib/Vector2.cpp
+++ Direct2D/Direct2D_EngineLib/Vector2.cpp
@@ -1,9 +1,37 @@
 #include "Vector2.h"
 
-Vector2::Vector2() : x(0), y(0) {}
+// static member init
+const Vector2 Vector2::zero = Vector2(0.0f, 0.0f);
+const Vector2 Vector2::one = Vector2(1.0f, 1.0f);
+const Vector2 Vector2::up = Vector2(0.0f, 1.0f);
+const Vector2 Vector2::down = Vector2(0.0f, -1.0f);
+const Vector2 Vector2::left = Vector2(-1.0f, 0.0f);
+const Vector2 Vector2::right = Vector2(1.0f, 0.0f);
 
-Vector2::Vector2(float x, float y) : x(x), y(y) {}
+// static function
+float Vector2::Dot(const Vector2& a, const Vector2& b)
+{
+    return a.x * b.x + a.y * b.y;
+}
 
+Vector2 Vector2::Lerp(const Vector2& a, const Vector2& b, float t)
+{
+    return a + (b - a) * t;
+}
+
+float Vector2::Distance(const Vector2& a, const Vector2& b)
+{
+    return (a - b).Magnitude();
+}
+
+float Vector2::Angle(const Vector2& from, const Vector2& to)
+{
+    float dot = Dot(from.Normalized(), to.Normalized());
+    dot = std::fmax(std::fmin(dot, 1.0f), -1.0f); // Clamp
+    return std::acos(dot) * (180.0f / 3.14159265358979323846f); // rad -> deg
+}
+
+// functions
 Vector2 Vector2::operator+(const Vector2& other) const
 {
     return Vector2(x + other.x, y + other.y);
--- Direct2D/Direct2D_EngineLib/Vector2.h
+++ Direct2D/Direct2D_EngineLib/Vector2.h
@@ -8,8 +8,22 @@ public:
     float y;
 
     // construct
-    Vector2();
-    Vector2(float x, float y);
+    Vector2() : x(0.0f), y(0.0f) {}
+    Vector2(float x, float y) : x(x), y(y) {}
+
+    // static constants (unity like)
+    static const Vector2 zero;
+    static const Vector2 one;
+    static const Vector2 up;
+    static const Vector2 down;
+    static const Vector2 left;
+    static const Vector2 right;
+
+    // static function
+    static float Dot(const Vector2& a, const Vector2& b);
+    static Vector2 Lerp(const Vector2& a, const Vector2& b, float t);
+    static float Distance(const Vector2& a, const Vector2& b);
+    static float Angle(const Vector2& from, const Vector2& to);
 
     // operator overloading
     Vector2 operator+(const Vector2& other) const;
@@ -22,7 +36,7 @@ public:
     Vector2& operator*=(float scalar);
     Vector2& operator/=(float scalar);
 
-    // func
+    // function
     float Magnitude() const;
     Vector2 Normalized() const;
 };
