1c5ffe0383477ded30718fc0f99b533378547f55
woojeong <77192050+wooj22@users.noreply.github.com>
2025-08-22 18:42:26 +0900

    TODO :: 2D 데모 프로젝트 개발

---
+++ Friend_Engine/Demo2D_Project/DemoApp.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../Friend_2D_EngineLib/GameApp.h"
+
+class DemoApp : public GameApp
+
+{
+};
+
---
+++ Friend_Engine/Demo2D_Project/Main.cpp
@@ -0,0 +1,8 @@
+#include "DemoApp.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+
+
+    return (int)1;
+}
